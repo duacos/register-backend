fb28129ec60c15311a357e81312793110732c278
Jorge Durango <djorge.ivan97@gmail.com>
2020-02-12 09:45:46 -0500

    added show action

--- app/controllers/api/v1/empresas_controller.rb
+++ app/controllers/api/v1/empresas_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::EmpresasController < ApplicationController
     @empresas = Empresa.all
     render json: @empresas, include: [:politica]
   end
+
+  def show
+    @empresa = Empresa.find(params[:id])
+    render json: @empresa, include: [:politica]
+  end
 end
