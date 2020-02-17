b3cca77847da18f9af8dc04ecc4107a1f37b7951
Jorge Durango <djorge.ivan97@gmail.com>
2020-02-17 10:05:01 -0500

    added all actions to empresas

--- app/controllers/api/v1/empresas_controller.rb
+++ app/controllers/api/v1/empresas_controller.rb
@@ -1,12 +1,48 @@
 class Api::V1::EmpresasController < ApplicationController
+  before_action :set_empresa, only: [ :show, :update, :destroy]
+  before_action :set_models, only: [:index, :show]
 
   def index 
     @empresas = Empresa.all
-    render json: @empresas, include: [:politica]
+    render json: @empresas, include: @models
   end
 
   def show
-    @empresa = Empresa.find(params[:id])
-    render json: @empresa, include: [:politica]
+    render json: @empresa, include: @models
   end
+
+
+  def create
+    @empresa = Empresa.new(empresa_params)
+    if @empresa.save
+      render json: @empresa, status: :created
+    else 
+      render json: @empresa.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @empresa.update(empresa_params)
+      render json: @empresa
+    else 
+      render json: @empresa.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @empresa.destroy
+  end
+
+  private 
+
+    def set_empresa
+      @empresa = Empresa.find(params[:id])
+    end
+
+    def set_models
+      @models = [:politica]
+    end
+    def empresa_params
+      params.require(:empresa).permit(:name, :description)
+    end
 end
